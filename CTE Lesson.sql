a2d2a2d96bde6cd25bdb0b5335c781d15ce0c57a
Mark Wilcock <mark.wilcock@zomalex.co.uk>
2026-05-12 18:41:17 +0100

    Made small improvements with the help of Claude

--- CTE Lesson.sql
+++ CTE Lesson.sql
@@ -1,78 +1,79 @@
 /*
 SQL Course - CTE Lesson
-CTE - Common Table Expression,uses the WITH clause
+CTE - Common Table Expression, uses the WITH clause
 */
 
 /*
 Two (or more step) processes are tricky when we have to write everything in one statement
 One option is a simple subquery approach
-Note that this approach may becomes unreadable if there are several levels of nesting
+Note that this approach may become unreadable if there are several levels of nesting
 */
 
 SELECT
-	p.AdmittedDate
-	,p.AdvancedTariff
+    p.AdmittedDate
+    ,p.AdvancedTariff
 FROM
-	(
-	SELECT
-		ps.AdmittedDate
-		,ps.Hospital
-		,SUM(ps.Tariff) + 1 AS AdvancedTariff
-	FROM
-		PatientStay ps
-	GROUP BY
-		ps.AdmittedDate
-		,ps.Hospital) p
+    (
+    SELECT
+        ps.AdmittedDate
+        ,ps.Hospital
+        ,SUM(ps.Tariff) + 1 AS AdvancedTariff
+    FROM
+        PatientStay ps
+    GROUP BY
+        ps.AdmittedDate
+        ,ps.Hospital
+    ) p
 WHERE
-	p.Hospital = 'PRUH'
-	AND p.AdvancedTariff > 5;
+    p.Hospital = 'PRUH'
+    AND p.AdvancedTariff > 5;
 
 /*
-Use WITH and a common table expression (CTE) 
+Use WITH and a common table expression (CTE)
 This yields exactly the same result but is more readable.
-NOTE: SQL needs to know that WITH is the start of a statement - the previous statement must have a semi-colon at end
+NOTE: the statement before a WITH must end with a semi-colon, so SQL knows that WITH starts a new statement
 */
 
-WITH p (AdmittedDate,Hospital,AdvancedTariff)
-AS (
+WITH p (AdmittedDate, Hospital, AdvancedTariff) AS (
 SELECT
-	ps.AdmittedDate
-	,ps.Hospital
-	,SUM(ps.Tariff) + 1
+    ps.AdmittedDate
+    ,ps.Hospital
+    ,SUM(ps.Tariff) + 1
 FROM
-	PatientStay ps
+    PatientStay ps
 GROUP BY
-	ps.AdmittedDate
-	,ps.Hospital)
+    ps.AdmittedDate
+    ,ps.Hospital
+)
 SELECT
-	p.AdmittedDate
-	,p.AdvancedTariff
+    p.AdmittedDate
+    ,p.AdvancedTariff
 FROM
-	p
+    p
 WHERE
-	p.Hospital = 'PRUH'
-	AND p.AdvancedTariff > 5;
+    p.Hospital = 'PRUH'
+    AND p.AdvancedTariff > 5;
 
 /*
-We can also write this without the column list in the CTE as long as all the columns 
+We can also write this without the column list in the CTE as long as all the columns
 within the CTE definition have a column name or alias.
 */
-WITH p
-AS (
+WITH p AS (
 SELECT
-	ps.AdmittedDate
-	,ps.Hospital
-	,SUM(ps.Tariff) + 1 AS AdvancedTariff
+    ps.AdmittedDate
+    ,ps.Hospital
+    ,SUM(ps.Tariff) + 1 AS AdvancedTariff
 FROM
-	PatientStay ps
+    PatientStay ps
 GROUP BY
-	ps.AdmittedDate
-	,ps.Hospital)
+    ps.AdmittedDate
+    ,ps.Hospital
+)
 SELECT
-	p.AdmittedDate
-	,p.AdvancedTariff
+    p.AdmittedDate
+    ,p.AdvancedTariff
 FROM
-	p
+    p
 WHERE
-	p.Hospital = 'PRUH'
-	AND p.AdvancedTariff > 5;
+    p.Hospital = 'PRUH'
+    AND p.AdvancedTariff > 5;
